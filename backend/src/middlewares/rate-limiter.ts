440230082c6a9efc0a005416e322a1e94defb8e4
Alexander Kiselev <pumad456@gmail.com>
2026-06-03 19:13:17 +0200

    исправления по автотестам без аудита

--- backend/src/middlewares/rate-limiter.ts
+++ backend/src/middlewares/rate-limiter.ts
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    limit: 100,
+    windowMs: 60 * 1000,
+    limit: 10,
     standardHeaders: true,
     legacyHeaders: false,
 })
